b76e666444f897880b6edf0a093b069e1f42e72b
rallierod <99930994+rallierod@users.noreply.github.com>
2022-10-25 23:11:26 -0500

    Finished testing for Job class (passed test 4)

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,46 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job testJob;
+    Job testJob2;
+@Test
+   public void testSettingJobId(){
+    testJob = new Job();
+    testJob2 = new Job();
+    assertNotEquals(testJob.getId(), testJob2.getId());
+}
 
+@Test
+    public void testJobConstructorSetsAllFields(){
+    testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    assertTrue(testJob.getName() instanceof String);
+    assertTrue(testJob.getEmployer() instanceof Employer);
+    assertTrue(testJob.getLocation() instanceof Location);
+    assertTrue(testJob.getPositionType() instanceof PositionType);
+    assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+    assertEquals("Product tester", testJob.getName());
+    assertEquals(testJob.getEmployer().getValue(), "ACME");
+    assertEquals(testJob.getLocation().getValue(), "Desert");
+    assertEquals(testJob.getPositionType().getValue(), "Quality control");
+    assertEquals(testJob.getCoreCompetency().getValue(), "Persistence");
+}
+@Test
+
+    public void testJobsForEquality() {
+    testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+    assertFalse(testJob.getId() == testJob2.getId());
+}
 }
